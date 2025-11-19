ebadd62c92c6e976725c98f09daae22730e7e7ba
João Reinaldo <jreinaldo@abbc.org.br>
2025-11-19 16:43:37 -0300

    Feat: adicionando os campos da entidade usuario

--- src/main/java/app/Homi/HomiApp/model/userModel.java
+++ src/main/java/app/Homi/HomiApp/model/userModel.java
@@ -1,11 +1,13 @@
 package app.Homi.HomiApp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.math.BigInteger;
+import java.time.LocalDateTime;
 
 @Entity
 @Table( name = "users")
@@ -14,4 +16,22 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class userModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private BigInteger id;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "password_hash")
+    private String password;
+    @Column(name = "avatar_url")
+    private String fotoURL;
+    @Column(name = "phone")
+    private String celular;
+    @Column(name = "created_at")
+    private LocalDateTime created;
+    @Column(name = "last_login")
+    private LocalDateTime lastLogin;
 }
