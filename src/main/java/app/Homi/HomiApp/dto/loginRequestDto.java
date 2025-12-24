87125ef9ea62949fd23c300737e21d8ae8d35a0b
JoaoHenriqueDev <joaohmreinaldo@gmail.com>
2025-12-24 11:22:33 -0300

    Feat: criação dos DTOs de resposta de login e atualização das contas de usuario

---
+++ src/main/java/app/Homi/HomiApp/dto/loginRequestDto.java
@@ -0,0 +1,4 @@
+package app.Homi.HomiApp.dto;
+
+public record loginRequestDto(String email, String password) {
+}
---
+++ src/main/java/app/Homi/HomiApp/dto/userRequestUpdateDto.java
@@ -0,0 +1,4 @@
+package app.Homi.HomiApp.dto;
+
+public record userRequestUpdateDto(String email, String name, String fotoUrl, String celular) {
+}
