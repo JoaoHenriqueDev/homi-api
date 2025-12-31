be11003968af843f8bbc95b6f4cb724e2cbb2125
JoaoHenriqueDev <joaohmreinaldo@gmail.com>
2025-12-31 11:53:41 -0300

    "Fix: fazendo algumas anterações e melhorias nos EndPoins já existentes tanto de codigo quanto de nomenclaturas"

--- src/main/java/app/Homi/HomiApp/controller/familyController.java
+++ src/main/java/app/Homi/HomiApp/controller/familyController.java
@@ -1,12 +1,17 @@
 package app.Homi.HomiApp.controller;
 
 import app.Homi.HomiApp.dto.familyRequestDto;
+import app.Homi.HomiApp.dto.familyRequestUpdateDto;
 import app.Homi.HomiApp.dto.familyResponseDto;
+import app.Homi.HomiApp.dto.familyResponseListDto;
+import app.Homi.HomiApp.security.userDetailsImpl;
 import app.Homi.HomiApp.service.familyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -16,32 +21,32 @@ public class familyController  {
     private final familyService familyService;
 
     @PostMapping("/create")
-    public ResponseEntity<familyResponseDto> criarGrupo(@RequestBody familyRequestDto familyRequestDto){
-        familyResponseDto group = familyService.criarGrupo(familyRequestDto);
+    public ResponseEntity<familyResponseDto> criarGrupo(@AuthenticationPrincipal userDetailsImpl user, @RequestBody familyRequestDto familyRequestDto){
+        familyResponseDto group = familyService.criarGrupo(user.getId(),familyRequestDto);
         return ResponseEntity.ok(group);
     }
 
     @PostMapping("/enter")
-    public ResponseEntity<familyResponseDto> entrarNoGrupo(@RequestParam UUID invite, @RequestBody UUID idUser) throws Exception {
-        familyService.entrarNoGrupo(invite, idUser);
+    public ResponseEntity<familyResponseDto> entrarNoGrupo(@RequestParam UUID invite, @AuthenticationPrincipal userDetailsImpl user) throws Exception {
+        familyService.entrarNoGrupo(invite, user.getId());
         return ResponseEntity.accepted().build();
     }
 
-    @PutMapping("/update")
-    public ResponseEntity<familyResponseDto> atualizarGroup(@RequestBody UUID idAdmin, @RequestParam UUID id, @RequestParam familyRequestDto familyRequestDto){
-        familyResponseDto group = familyService.atualizarDadosGrupo(idAdmin, id,familyRequestDto);
+    @PutMapping("/update/{idGroup}")
+    public ResponseEntity<familyResponseDto> atualizarGroup(@AuthenticationPrincipal userDetailsImpl user, @PathVariable UUID idGroup, @RequestBody familyRequestUpdateDto familyRequestDto){
+        familyResponseDto group = familyService.atualizarDadosGrupo(user.getId(), idGroup,familyRequestDto);
         return ResponseEntity.ok(group);
     }
 
-    @GetMapping("/search")
-    public ResponseEntity<familyResponseDto> buscarGroup(@RequestParam String name){
-        familyResponseDto group = familyService.buscarPorNome(name);
-        return ResponseEntity.ok(group);
+    @GetMapping
+    public ResponseEntity<List<familyResponseListDto>> listarGrupos(@AuthenticationPrincipal userDetailsImpl user){
+        List<familyResponseListDto> dadosGrupos = familyService.listarGrupos(user.getId());
+        return ResponseEntity.ok(dadosGrupos);
     }
 
-    @DeleteMapping("/delete/user")
-    public ResponseEntity<familyResponseDto> deleteUserGroup(@RequestBody UUID idAdmin, @RequestParam UUID idUser, @RequestParam UUID idGroup){
-        familyService.deletarUsuario(idAdmin,idUser,idGroup);
+    @DeleteMapping("/delete/{idGroup}/user/{idUser}")
+    public ResponseEntity<familyResponseDto> deleteUserGroup(@AuthenticationPrincipal userDetailsImpl user, @PathVariable UUID idUser, @PathVariable UUID idGroup){
+        familyService.deletarUsuario(user.getId(),idUser,idGroup);
         return ResponseEntity.accepted().build();
     }
 }
--- src/main/java/app/Homi/HomiApp/controller/userController.java
+++ src/main/java/app/Homi/HomiApp/controller/userController.java
@@ -4,9 +4,11 @@ import app.Homi.HomiApp.dto.loginRequestDto;
 import app.Homi.HomiApp.dto.userRequestDto;
 import app.Homi.HomiApp.dto.userRequestUpdateDto;
 import app.Homi.HomiApp.dto.userResponseDto;
+import app.Homi.HomiApp.security.userDetailsImpl;
 import app.Homi.HomiApp.service.userService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -17,27 +19,15 @@ import java.util.UUID;
 public class userController {
     private final userService service;
 
-    @PostMapping("/register")
-    public ResponseEntity<userResponseDto> registrarUsuario(@RequestBody userRequestDto userRequestDto){
-        userResponseDto user = service.cadastrarUsuario(userRequestDto);
-        return ResponseEntity.ok(user);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<userResponseDto> loginUsuario(@RequestBody loginRequestDto loginRequestDto){
-        userResponseDto user = service.loginUsuario(loginRequestDto);
-        return ResponseEntity.ok(user);
-    }
-
     @PutMapping("/update")
-    public ResponseEntity<userResponseDto> updateUsuario(@RequestParam UUID id, @RequestBody userRequestUpdateDto userRequestUpdateDto){
-        userResponseDto user = service.atualizarUsuario(id, userRequestUpdateDto);
-        return ResponseEntity.ok(user);
+    public ResponseEntity<userResponseDto> updateUsuario(@AuthenticationPrincipal userDetailsImpl user, @RequestBody userRequestUpdateDto userRequestUpdateDto){
+        userResponseDto response = service.atualizarUsuario(user.getId(), userRequestUpdateDto);
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<String> deletarUsuario(@RequestParam UUID id){
-        service.deletarUsuario(id);
+    public ResponseEntity<String> deletarUsuario(@AuthenticationPrincipal userDetailsImpl user){
+        service.deletarUsuario(user.getId());
         return ResponseEntity.ok("Usuario apagado");
     }
 }
