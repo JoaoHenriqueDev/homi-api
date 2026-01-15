978343d9fe9191b3c31e89aadf943ef0af233e1a
Joao1218 <joaohmreinaldo@gmail.com>
2026-01-15 16:29:03 -0300

    Fix: arruma bug de prod

--- src/main/java/app/Homi/HomiApp/service/authService.java
+++ src/main/java/app/Homi/HomiApp/service/authService.java
@@ -15,7 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
+import java.lang.String;
 import java.util.List;
 
 @AllArgsConstructor
