caa49550ff80000096d8a04e3940959b278c2f86
Joao1218 <joaohmreinaldo@gmail.com>
2026-01-16 09:45:48 -0300

    Fix: arrumando o securitycondig pois não estava liberando o endpoint register, dava codigo 403 forbiden

--- src/main/java/app/Homi/HomiApp/security/securityConfig.java
+++ src/main/java/app/Homi/HomiApp/security/securityConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -34,7 +33,8 @@ public class securityConfig {
         return http
                 .csrf(csrf -> csrf.ignoringRequestMatchers(request ->
                         "POST".equalsIgnoreCase(request.getMethod())
-                                && "/auth/register".equals(request.getServletPath())
+                                && request.getRequestURI() != null
+                                && request.getRequestURI().endsWith("/auth/register")
                 ))
                 .cors(Customizer.withDefaults())
                 .sessionManagement(session ->
